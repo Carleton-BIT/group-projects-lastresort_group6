8fb37d8f73d53261cdf8e7667c4b8ca3572c494e
DelfinZh <delfin.hzhang@gmail.com>
2026-03-01 23:14:50 -0500

    login verifier split with registration

--- infoSave.php
+++
@@ -1,9 +0,0 @@
-<?php
-$email=htmlspecialchars($_POST["email"]);
-$password=htmlspecialchars($_POST["password"]);
-
-$conn = new mysqli("DelfinZ", "root", "LastResort6?","AccInfo");
-
-$sql = "INSERT INTO AccInfo (email, pass) VALUES ($email, $pass)";
-?>
-
---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $email=htmlspecialchars($_POST["email"]);
+  $password=htmlspecialchars($_POST["password"]);
+  $hashedPass=password_hash($password, PASSWORD_DEFAULT)
+
+  $conn = new mysqli("localhost", "root", "LastResort6?","AccInfo");
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "INSERT INTO AccInfo (email, pass) VALUES ($email, $pass)";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Account Created";
+  }
+}
+?>
+
